317efcc03b62ba08725c8f67d74d268e95db2288
Kmilia <camelia.guerraoui@gmail.com>
2023-10-12 15:28:15 +0900

    Add Navbar and redirects

---
+++ navbar.js
@@ -0,0 +1,9 @@
+
+function switchNavBar() {
+var x = document.getElementById("navSmall");
+if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+} else { 
+    x.className = x.className.replace(" w3-show", "");
+}
+}
